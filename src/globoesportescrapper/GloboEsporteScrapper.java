81f298b5e219e8d2fb03d88039eda0e6ec5aad51
Victor D. S. <vdsantiago.1@gmail.com>
2017-06-18 00:21:26 -0300

    Round scrapping sample

--- src/globoesportescrapper/GloboEsporteScrapper.java
+++ src/globoesportescrapper/GloboEsporteScrapper.java
@@ -5,6 +5,14 @@
  */
 package globoesportescrapper;
 
+import globoesportescrapper.model.interfaces.GloboEsporte;
+import globoesportescrapper.model.objects.Partida;
+import globoesportescrapper.model.objects.Rodada;
+import globoesportescrapper.model.scrapper.Scrapper;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Victor Santiago
@@ -15,7 +23,20 @@ public class GloboEsporteScrapper {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        GloboEsporte ge = new Scrapper();
+        
+        Rodada r = new Rodada();
+        try {
+            r = ge.getRodada("http://globoesporte.globo.com/servico/esportes_campeonato/responsivo/widget-uuid/c6008860-b121-40a5-afe6-3bae9af08a32/fases/fase-unica-seriea-2017/rodada/4/jogos.html");
+        } catch (IOException ex) {
+            Logger.getLogger(GloboEsporteScrapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        System.out.println("Rodada " + r.getNumber());
+        
+        for(Partida p : r.getPartidas())
+            System.out.println(p.getHome() + " " + p.getHomeGoals() + "x" + 
+                    p.getAwayGoals() + " " + p.getAway());
     }
     
 }
